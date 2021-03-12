8ed8c0dddecf738208c391f7dbc22486d4298532
patrick ojunde <patrickojunde@gmail.com>
2021-03-12 08:11:47 +0100

    updates

--- 19 advanced-generics/src/main/java/service/ProductService.java
+++ 19 advanced-generics/src/main/java/service/ProductService.java
@@ -11,34 +11,6 @@ import java.util.List;
 @Data
 public class ProductService {
 
-    /*****
-     * Arrays are covariant
-     * Generics are invariant:
-     * in Generic collections, you cam only assign a collection of the type specified as variable
-     * e.g
-     * List<A> listA = new ArrayList<B>(); wrong : even though B is a subTYpe of A, i.e inherit from A
-     * List<C> listC = new ArrayList<C> : this is correct
-     *
-     *
-     * WildCard Generics:
-     * Generics are used to enforce compile-time verification of a type.
-     * Generics are often used with Collection API. Consider the following collection examples:
-     *
-     * When `generics are not applied`: code defaults to use type Object
-     * - Only Object class operation can be safely used.
-     * - Type-check and type-casting must be applied to access any sub-type specific operations
-     *
-     * When `specific type is applied`:
-     * - Any operations declared for this type or its parents can be safely used
-     * - Type-check and type-casting must be applied to access any sub-type specific operations
-     *
-     * When `wildcard <?> is applied`: (representing an unknown type):
-     * - Elements are accessed just like in a collection of Objects
-     * - Effectively it's a read-only collection
-     * - No object in Java is of unkown type, so no values (except null) can be added to such a collection
-     *
-     *
-     */
 
 
     // singleton
